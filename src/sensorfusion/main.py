8ecbf16479c314742bc8e5b01aaded6fca4cb60b
Zaheen <zaheenmohammed000@gmail.com>
2026-08-06 15:45:35 +0530

    Cam import correction

--- src/sensorfusion/main.py
+++ src/sensorfusion/main.py
@@ -1,5 +1,5 @@
 from forward import ESIKFStateEstimator
-from initialize import Lidar, IMU, Camera
+from initialize import Lidar, IMU, CameraSensor
 import numpy as np
 
 if __name__ == "__main__":
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     initial_state = np.zeros(18)
     initial_covariance = 100 * np.eye(18)
     imu = IMU()
-    
+
     while(True):
         imu_data = imu.get_readings()
         state, cov = filter.predict(imu_data)
