Fix: empty compensted points. Camera photometric error calculation.

src/sensorfusion/lidar/backward.py
@@ -78,4 +78,4 @@ def backprop(scan_end_time, prev_scan_time, imu_pose, scan, imu_measurement_buff
         # print("p_kj: ", p_kj)
         projected_point = R_kj @ point_body + p_kj
         compensated_points.append(projected_point)
-    return compensated_points
+    return np.asarray(compensated_points, dtype=float).reshape(-1, 3)

src/sensorfusion/main.py
@@ -6,7 +6,7 @@ from lidar.backward import backprop
 from lidar.update import lidar_acquisition_process, lidar_thread
 from map import Map
 from utils.so3_rotation import skew, exp
-from utils.projections import project_points_to_frame, project_points_world
+from utils.projections import project_points_to_frame, project_points_world, calculate_photometric_error
 from threading import Thread,Lock
 from multiprocessing import Process, Queue
 from collections import deque
@@ -162,25 +162,6 @@ if __name__ == "__main__":
 
     while(True):
 
-        # #Get the camera scan at 10Hz
-        # frame = cam.get_frame()
-        
-        # #find visual map points for the current frame based on current pose and current lidar scan
-        # visual_map_points = map.query_visible_voxels(lidar_scan_queue, sta) #visible voxel query
-
-        # #project lidar points to the current camera frame (u,v)
-        # R_CI = np.eye(3) 
-        # t_CI = np.eye(3)
-
-        # T_CI = np.eye(4) #dummy camera imu extrinsics, real values have to be calibrated later
-        # T_CI[:3, :3] = R_CI
-        # T_CI[:3, 3] = t_CI
-
-        # projected_points = project_points_to_frame(visual_map_points, T_GI, T_CI)
-        # #get the 8x8 pixel patch surrounding the current lidar point
-        # #attach the patch to the lidar map point
-
-        # print(frame)
         now = time.time()
         if now - prev_time >= 1:
             prev_time = now
@@ -188,34 +169,45 @@ if __name__ == "__main__":
                 state = filter.state
                 print("Current state (x,y,z): ", state.p)
 
-# def camera_thread(cam, state_lock, filter, map, imu_measurement_buffer, lidar_scan_queue):
-#         #Get the camera scan at 10Hz
-#         frame = cam.get_frame()
+def camera_thread(cam, state_lock, filter, map, imu_measurement_buffer, lidar_scan_queue):
+        #Get the camera scan at 10Hz
+        frame = cam.get_frame()
         
-#         #find visual map points for the current image frame based on current pose and current lidar scan
-#         visual_map_points = map.query_visible_voxels(lidar_scan_queue, filter.state) #visible voxel query
+        #find visual map points for the current image frame based on current pose and current lidar scan
+        visual_map_points = map.query_visible_voxels(lidar_scan_queue, filter.state) #visible voxel query
 
-#         #project lidar points to the current camera frame (u,v)
-#         R_CI = np.eye(3) 
-#         t_CI = np.eye(3)
+        #project lidar points to the current camera frame (u,v)
+        R_CI = np.eye(3) 
+        t_CI = np.eye(3)
 
-#         T_CI = np.eye(4) #dummy camera imu extrinsics, real values have to be calibrated later
-#         T_CI[:3, :3] = R_CI
-#         T_CI[:3, 3] = t_CI
+        T_CI = np.eye(4) #dummy camera imu extrinsics, real values have to be calibrated later
+        T_CI[:3, :3] = R_CI
+        T_CI[:3, 3] = t_CI
 
-#         projected_points_pixels = project_points_to_frame(visual_map_points, T_GI, T_CI)
-#         #get the 8x8 pixel patch surrounding the current lidar point
-#         for point in projected_points_pixels:
-#             #get the 8x8 patch surrounding the pixel
-#             pixel = point[1]
-#             u = pixel[0]
-#             v = pixel[1]
-#             patch = frame[u-4:u+4, v-4:v+4]
-#             #get the voxel for the current lidar point
-#             #attach the patch to the lidar map point
-#             map.add_visual_patch(point, patch)
+        projected_points_pixels = project_points_to_frame(visual_map_points, T_GI, T_CI)
 
-#         print(frame)
-#         with state_lock:
-#             state = filter.state
-#             print("Current state (x,y,z): ", state.p)
+        residual_list =[]
+
+        #get the 8x8 pixel patch surrounding the current lidar point
+        for point in projected_points_pixels:
+            #get the 8x8 patch surrounding the pixel
+            pixel = point[1]
+            u = pixel[0]
+            v = pixel[1]
+            current_patch = frame[u-4:u+4, v-4:v+4]
+
+            reference_patch = map.get_reference_patch(point)
+
+            #if the current point doesnt have a patch add the new patch and continue
+            if reference_patch is None:
+                #attach the patch to the lidar map point
+                map.add_visual_patch(point, current_patch)
+                continue
+
+            #calculate the photometric residual for each of the points
+            residual = calculate_photometric_error(current_patch, reference_patch)
+
+            #add the point residual to the total residual
+            residual_list.append(residual)
+        #do the camera based update using the residual and Kalman Gain
+        #